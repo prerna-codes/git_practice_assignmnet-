3374ffff87a4ab049306e6783e94d315f3dd6c31
None <prernacodes@gmail.com>
2022-10-22 21:17:18 +0530

    Programs

---
+++ palindrome.js
@@ -0,0 +1,9 @@
+str="abbba"
+let rev=str.toString().split("").reverse().join("");
+   let mystr=str.toString();
+   if(mystr==rev){
+       console.log("Yes")
+   }
+   else{
+       console.log("No")
+   }
